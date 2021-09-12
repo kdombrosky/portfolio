7fdfb7e6ca20c207cb58be6a01909b049f7962e1
Kayla Dombrosky <kdombroskyy@live.com>
2021-09-11 23:44:52 -0400

    navbar code cleanup

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,19 +1,15 @@
 import React, { useEffect } from 'react'; 
-import { capitalizeFirstLetter } from "../../utils/helpers";
 import logo from '../../assets/logo2.png';
 
 function Nav(props) {
     const {
         categories = [],
         setCurrentCategory,
-        currentCategory,
-        // contactSelected,
-        // setContactSelected
+        currentCategory
     } = props;
 
-    // 
     useEffect(() => {
-        document.title = capitalizeFirstLetter(currentCategory.name);
+        document.title = (currentCategory.name);
     }, [currentCategory]);
 
     return(
@@ -27,31 +23,18 @@ function Nav(props) {
                     />
                 </a>
                     <ul className="navbar-nav mb-2 d-flex">
-                        {/* <li>
-                            <a data-testid="about" href="#about">About me</a>
-                        </li>
-                        <li>
-                            <a data-testid="about" href="#about">Portfolio</a>
-                        </li>
-                        <li>
-                            <a data-testid="about" href="#about">Contact</a>
-                        </li>
-                        <li>
-                            <a data-testid="about" href="#about">Resume</a>
-                        </li> */}
                         {categories.map((category) => (
-                        // Set class to 'navActive' if the current category has been selected 
+                        // Set class to 'active' if the current category has been selected 
                         <li
                         className={`nav-item mx-2 fs-5 ${currentCategory.name === category.name && `active`}`}
                         key={category.name}
-                        // href={`#${category.name}`}
                         data-testid={`#${category.name}`}
                         >
                             <span onClick={() => { setCurrentCategory(category) }}>
                             <a 
                             href={`#${category.name}`}
                             className='nav-link'>
-                                {capitalizeFirstLetter(category.name)}
+                                {category.name}
                             </a>
                             </span>
                         </li>
